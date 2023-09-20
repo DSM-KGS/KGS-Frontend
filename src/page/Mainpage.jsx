dd6ada4f3bc2e7ae124ebd9cbe6244e115a70a18
Choiheeyoung <choiheeyoung10@gmail.com>
2023-09-20 22:10:40 +0900

    [cre] main

--- src/page/Mainpage.jsx
+++ src/page/Mainpage.jsx
@@ -5,7 +5,19 @@ import axios from "axios";
 function MainPage() {
   return (
     <>
-      <S.Background></S.Background>
+      <S.Background>
+        <S.font>KGS</S.font>
+        <S.font_en>
+          Kisugsa tabang
+          <br />
+          Geumji System
+        </S.font_en>
+        <S.font_kr>기숙사 타방 금지 시스템</S.font_kr>
+        <S.button>LOG IN</S.button>
+        <S.main_background src="img/main_background.png"></S.main_background>
+        <S.main_lock src="img/lock.png"></S.main_lock>
+        <S.main_shadow src="img/shadow.png"></S.main_shadow>
+      </S.Background>
     </>
   );
 }
