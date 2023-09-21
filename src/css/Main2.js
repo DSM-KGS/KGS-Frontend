0cd217e53253554b62ec5cdcf2db031c6b8547f8
Choiheeyoung <choiheeyoung10@gmail.com>
2023-09-21 10:06:02 +0900

    [ret] main login

---
+++ src/css/Main2.js
@@ -0,0 +1,71 @@
+import { styled } from "styled-components";
+
+export const Background = styled.div`
+  width: 100%;
+  background: linear-gradient(
+      270deg,
+      rgba(108, 74, 182, 0.2) 0%,
+      rgba(225, 219, 241, 0) 50%
+    ),
+    #ffffff;
+  display: flex;
+  padding-bottom: 63px;
+`;
+
+export const alarm = styled.img`
+  width: 680px;
+  margin: 30px 0px 0px 70px;
+`;
+
+export const main_left = styled.div``;
+
+export const main_right = styled.div`
+  margin: 76px 0px 0px 85px;
+`;
+
+export const main_right_sort = styled.div`
+margin: 0px 0px 30px 0px;
+`;
+
+export const box = styled.div`
+  width: 924px;
+  height: 130px;
+  background-color: white;
+  border: solid 0.5px #6c4ab6;
+  border-radius: 20px;
+  box-shadow: 15px 15px 4px #6c4ab6;
+  margin: 20px 0px 0px 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const main_div = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const font = styled.div`
+  font-size: 35px;
+  font-weight: 800;
+  color: #8d72e1;
+`;
+
+export const alarmImg = styled.img`
+  width: 100px;
+  margin-left: 20px;
+`;
+
+export const main_content = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: #8d72e1;
+  margin-left: 20px;
+`;
+
+export const date = styled.div`
+  font-size: 15px;
+  color: #a79dbb;
+  margin-right: 20px;
+`;
