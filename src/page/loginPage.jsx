17aac8480aa969b28b84f08ed7fb5cb2020469b7
hoseon07 <a01081930429@gmail.com>
2023-09-25 19:55:06 +0900

    [Update] login

--- src/page/loginPage.jsx
+++ src/page/loginPage.jsx
@@ -1,20 +1,21 @@
 import react from 'react';
 import styled from 'styled-components';
+import axios from "axios"
 
 function loginPage() {
   return (
     <>
       <Container>
-        <LoginBox>
+        <OutLine>
           <LoginImg src="img/login_background.png" />
+          <Box>
           <Subject>LOG IN</Subject>
-          <div>
             <Input type="text" placeholder="ID"/>
             <Input type="password" placeholder="PASSWORD"/>
             <Btn value="CONTINUE">CONTINUE</Btn>
             <P>비밀번호를 잊으셨다면 관리자에게 문의해주세요</P>
-          </div>
-          </LoginBox>
+          </Box>
+        </OutLine>
       </Container>
     </> 
   )
@@ -22,39 +23,51 @@ function loginPage() {
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
+  height: 855px;
   background: linear-gradient(270deg, rgba(108, 74, 182, 0.20) 0%, rgba(255, 255, 255, 0.20) 37.24%), #FFF;
 `
-const LoginBox = styled.div`
+const OutLine = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-left:272px;
   width: 1375px;
   height: 747px;
-  margin-left:272px;
   border-radius: 20px;
   background: #FFF;
   box-shadow: 0px 6px 20px 0px #6C4AB6;
 `
 const Subject = styled.p`
   color:#3C1D81;
+  margin:98px 0 73px 0;
   font-size: 40px;
   font-weight: 900;
   letter-spacing:8px;
 `
 const LoginImg = styled.img`
-  width:740px;
-  height: 747px;
+  width: 739px;
+  height: 774px;
+  margin-right: 32px;
+`
+const Box = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 77px;
+  float: right;
 `
 const Input = styled.input`
   display: flex;
   flex-direction:column;
   align-items: center;
+  margin-bottom: 53px;
   width: 524px;
   font-size:20px;
-  letter-spacing: 2px;
   font-weight: 500;
+  letter-spacing: 2px;
   border-top:none;
   border-right: none;
   border-left: none;
-  border-bottom: 2px solid linear-gradient(270deg, #B9E0FF 0%, rgba(185, 224, 255, 0.00) 50%), #6C4AB6;;
+  border-bottom: 2px solid linear-gradient(270deg, #B9E0FF 0%, rgba(185, 224, 255, 0.00) 50%), #6C4AB6;
   &:focus {
     outline:none;
   }
@@ -63,6 +76,7 @@ const Btn = styled.button`
   width: 524px;
   height: 71px;
   color:white;
+  margin: 202px 0 27px 0;
   font-size: 20px;
   font-weight: 500;
   letter-spacing: 8px;
